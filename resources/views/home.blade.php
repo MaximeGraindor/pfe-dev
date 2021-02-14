bfa3d98be135627557764537fb4535882151d9a9
Maxime Graindor <maxime.graindor@hotmail.com>
2021-02-14 21:21:29 +0100

    Start the home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,17 @@
 @extends('layouts.main')
 @section('content')
-    <p>
-        Hello world!
-    </p>
-    <a href="/login">Se connecter</a>
-    <a href="/register">S'inscrire</a>
+    <div class="content-size">
+        <header class="header">
+        	<h1 role="heading" aria-level="1" class="header-title">
+                Gameon
+            </h1>
+        	<nav class="header-menu">
+                <h2 role="heading" aria-level="2" class="hidden">
+                    Naviguation principal
+                </h2>
+        		<a href="/login">Se connecter</a>
+        		<a href="/register">S'inscrire</a>
+        	</nav>
+        </header>
+    </div>
 @endsection

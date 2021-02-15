5e0423f7864f9415a382e647606b43abd6fcadbe
Maxime Graindor <maxime.graindor@hotmail.com>
2021-02-15 22:10:20 +0100

    Add styles to login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,37 @@
 @extends('layouts.main')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="mb-0 form-group row">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+<div class="login-splitscreen">
+    <div class="login-left"></div>
+    <div class="login-right">
+        <section class="login-wrapper">
+            <h2 role="heading" aria-level="2"  class="login-title">
+                Connectez-vous
+            </h2>
+            <p class="login-intro">
+                Pas encore inscrit ? <a href="/register">S'inscrire</a>
+            </p>
+            <form action="/login" method="post" class="login-form">
+                @csrf
+                <div class="login-pseudo">
+                    <label for="pseudo">Pseudo</label>
+                    <input type="text" id="pseudo" name="pseudo">
                 </div>
-            </div>
-        </div>
+                <div class="login-password">
+                    <label for="password">Mot de passe</label>
+                    <input type="password" id="password" name="password">
+                    @if (Route::has('password.request'))
+                        <a class="login-forgotPassword" href="{{ route('password.request') }}">
+                            Mot de passe oublié&nbsp;?
+                        </a>
+                    @endif
+                </div>
+
+                <div class="login-submit">
+                    <input type="submit" value="Se connecter">
+                </div>
+            </form>
+        </section>
     </div>
 </div>
 @endsection

13c4acb4a3bed68800bdd8936e3fbffa3a88c6fc
Maxime Graindor <maxime.graindor@hotmail.com>
2021-02-23 16:50:54 +0100

    Add button for update profil

--- resources/views/pages/profil.blade.php
+++ resources/views/pages/profil.blade.php
@@ -6,7 +6,12 @@
         <div class="profil-header">
             <img src="./img/Login-background.jpg" alt="">
             <div class="profil-info">
-                <p class="profil-info-pseudo"> {{ Auth::user()->pseudo }}</p>
+                <p class="profil-info-pseudo">
+                    {{ Auth::user()->pseudo }}
+                    <a href="/profil/modifier" title="Modifier son profil" class="profil-info-update">
+                        <img src="./img/settings.svg" alt="">
+                    </a>
+                </p>
                 <div class="profil-activity">
                     <div class="profil-activity-card">
                         <p>En cours</p>
@@ -24,7 +29,30 @@
             </div>
         </div>
         <div class="profil-content">
-
+            <section class="profil-content-section">
+                <h2>
+                    En cours
+                </h2>
+                <p>
+                    Pas de jeux pour le moment
+                </p>
+            </section>
+            <section class="profil-content-section">
+                <h2>
+                    Terminé
+                </h2>
+                <p>
+                    Pas de jeux pour le moment
+                </p>
+            </section>
+            <section class="profil-content-section">
+                <h2>
+                    Liste d'envie
+                </h2>
+                <p>
+                    Pas de jeux pour le moment
+                </p>
+            </section>
         </div>
     </div>
 @endsection

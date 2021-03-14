942a88c5882010d1df80b23ae28f76aa8a71d99a
Maxime Graindor <maxime.graindor@hotmail.com>
2021-03-14 14:14:15 +0100

    Add games and fix css

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('pseudo')->nullable();
+            $table->string('pseudo');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('sexe')->default('');
-            $table->string('birtday')->default('');
             $table->rememberToken();
             $table->timestamps();
         });

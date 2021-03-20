431fd63cbda19efd04b89498858d97973ee6bc06
Maxime Graindor <maxime.graindor@hotmail.com>
2021-03-20 15:16:01 +0100

    Display the right games for each lists

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -11,19 +11,29 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        $currentGamesList = User::with('games')
+        /* $currentGamesList = User::with('games')
         ->where('id', Auth::user()->id)
         //->wherePivot('relation', 'en cours')
-        ->get();
-        $finishGamesList = User::with('games')
+        ->get(); */
+
+        $currentGamesList = User::with(['games' => function ($query){
+            $query->wherePivot('relation', 'en cours');
+        }])
         ->where('id', Auth::user()->id)
-        //->wherePivot('relation', 'termine')
         ->get();
-        $wishGamesList = User::with('games')
+
+        $finishGamesList = User::with(['games' => function ($query){
+            $query->wherePivot('relation', 'termine');
+        }])
         ->where('id', Auth::user()->id)
-        //->wherePivot('relation', 'envie')
         ->get();
-        //return $currentGamesList;
+
+        $wishGamesList = User::with(['games' => function ($query){
+            $query->wherePivot('relation', 'envie');
+        }])
+        ->where('id', Auth::user()->id)
+        ->get();
+
         return view('pages.activity', compact('currentGamesList', 'finishGamesList', 'wishGamesList'));
     }
 }
